a62f6274883c83fb92f5c59401463845974391b9
Will Spurgeon <will@spurgeonworld.com>
2022-01-23 20:15:18 -0500

    Basic solution

--- Spelling Bee Solver/main.swift
+++ Spelling Bee Solver/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+//let mainLetter = Character(CommandLine.arguments[1]).lowercased()
+//let followupLetters = CommandLine.arguments[2...7].reduce("", { $0 + $1.lowercased() })
 
+let mainLetter: Character = "p"
+let followupLetters = "yletgo"
+
+//let words = wordsRawString.split(separator: "\n")
+let words = moreWords.split(separator: "\n")
+let longWords = words.filter({ $0.count > 3 })
+
+let wordsWithMainLetter = longWords.filter { $0.contains(mainLetter) }
+let charSet = CharacterSet(followupLetters.unicodeScalars + mainLetter.unicodeScalars)
+
+let solutions = wordsWithMainLetter.filter { charSet.isSuperset(of: CharacterSet($0.unicodeScalars)) }
+
+for solution in solutions {
+    print(solution)
+}
